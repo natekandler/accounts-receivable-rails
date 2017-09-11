c281cdc15ef401e298444e52e909535d94c7c56a
Justin Searls <searls@gmail.com>
2017-09-10 20:25:48 -0400

    prevent nulls and run the migration generated by the scaffold

--- db/migrate/20170911001144_create_clients.rb
+++ db/migrate/20170911001144_create_clients.rb
@@ -1,8 +1,8 @@
 class CreateClients < ActiveRecord::Migration[5.1]
   def change
     create_table :clients do |t|
-      t.string :name
-      t.string :email
+      t.string :name, :null => false
+      t.string :email, :null => false
 
       t.timestamps
     end
