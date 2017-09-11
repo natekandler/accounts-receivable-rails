73a68eeeb22eb7c911a37975299e492ccd657a31
Justin Searls <searls@gmail.com>
2017-09-10 20:31:43 -0400

    rails g scaffold Product name:string price_cents:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911001144) do
+ActiveRecord::Schema.define(version: 20170911003131) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price_cents", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
