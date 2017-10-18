64fdf59bf4809a806982dd4f44d4d253020a6987
Justin Searls <searls@gmail.com>
2017-10-18 08:50:16 -0400

    Babby's first system test

--- test/system/clients_test.rb
+++ test/system/clients_test.rb
@@ -1,9 +1,47 @@
 require "application_system_test_case"
 
 class ClientsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit clients_url
-  #
-  #   assert_selector "h1", text: "Client"
-  # end
+  test "CRUDing a client" do
+    visit clients_url
+
+    # Create
+    click_link "New Client"
+    fill_in "Name", :with => "Widgets, Inc."
+    fill_in "Email", :with => "owners@widgets.biz"
+
+    click_button "Create Client"
+
+    assert_text "Client was successfully created."
+    assert_text "Name: Widgets, Inc."
+    assert_text "Email: owners@widgets.biz"
+
+    click_link "Back"
+
+    # Update
+    find_client_row_named("Widgets, Inc.").find_link("Edit").click
+    fill_in "Name", :with => "Widgets LLP"
+    fill_in "Email", :with => "partners@widgets.biz"
+
+    click_button "Update Client"
+
+    assert_text "Client was successfully updated."
+    assert_text "Name: Widgets LLP"
+    assert_text "Email: partners@widgets.biz"
+
+    click_link "Back"
+
+    # Destroy
+    find_client_row_named("Widgets LLP").find_link("Destroy").click
+
+    accept_confirm "Are you sure?"
+
+    assert_text "Client was successfully destroyed."
+    refute_text "Widgets"
+  end
+
+  private
+
+  def find_client_row_named(name)
+    find("tbody").all("tr").find { |tr| tr.first("td").text == name }
+  end
 end
