6c796b194388ed7083a8368c54357145ed01832c
Dustin Tinney <tinney@gmail.com>
2018-10-15 20:27:09 -0700

    Adds a test for the show of invoices controller

--- test/controllers/invoices_controller_test.rb
+++ test/controllers/invoices_controller_test.rb
@@ -7,7 +7,29 @@ class InvoicesControllerTest < ActionController::TestCase
     assert_equal Invoice.all, assigns(:invoices)
   end
 
-  def test_show
+  def test_show_assigns_a_client
+    invoice = invoices(:build)
 
+    get :show, params: { id: invoice.id, }
+
+    assert_equal clients(:acme), assigns(:client)
+  end
+
+  def test_show_assigns_line_items
+    invoice = invoices(:build)
+
+    get :show, params: { id: invoice.id, }
+
+    line_items = assigns(:line_items)
+    assert_equal line_items[:product].count, 1
+    assert_equal line_items[:service].count, 2
+  end
+
+  def test_show_assigns_a_total
+    invoice = invoices(:build)
+
+    get :show, params: { id: invoice.id, }
+
+    assert_equal 2_480_000, assigns(:total)
   end
 end
